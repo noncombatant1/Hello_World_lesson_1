2f6c58db985831a860e1f00ec539939ee5de28ef
noncombatant1 <dubovetsaleksey@gmail.com>
2021-10-29 00:37:51 +0600

    Add project files.

---
+++ Hello_World_lesson_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hi, {name} today is [{DateTime.Now}]");
+        }
+    }
+}
